8404e655b37f0ecf442fb18bfa7378dd1f0f97dd
shahruhabd <abdugapar04@gmail.com>
2023-12-25 10:57:47 +0600

    удалил не нужные файлы

--- helpdesk/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import HelpDeskRequest, Lecturer, HelpDeskUser
-
-admin.site.register(HelpDeskRequest)
-admin.site.register(Lecturer)
-admin.site.register(HelpDeskUser)
